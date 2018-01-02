7672ab8e736770d242d95684dd5f892b638bd7ff
Eddie Melo <heriberto@melo.nyc>
2018-01-02 22:49:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,22 +7,22 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    @@sides << [side1, side2, side3]
+    self.sides << [side1, side2, side3]
   end
 
   def kind
     validate_triangle
-    return :equilateral if @@sides[0] == @@sides[2]
-    return :isosceles if @@sides[0] == @@sides[1] or @@sides[1] == @@sides[2]
+    return :equilateral if self.sides[0] == self.sides[2]
+    return :isosceles if self.sides[0] == self.sides[1] or self.sides[1] == self.sides[2]
     return :scalene
   end
 
   def validate_triangle
     #place all the sides into an array and sort the array
-    @@sides.sort
+    self.sides.sort
     #raise the error if the lowest # is less than or equal to 0 or if the 2
     #lesser sides are less than or equal to the biggest side
-    raise TriangleError if @@sides[0] <= 0 or @@sides[0] + @@sides[1] <= @@sides[2]
+    raise TriangleError if self.sides[0] <= 0 or self.sides[0] + self.sides[1] <= self.sides[2]
   end
 end
 
