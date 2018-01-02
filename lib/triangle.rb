7d2cefad5eab416e0f1daf6275e1149063bcfcfa
Eddie Melo <heriberto@melo.nyc>
2018-01-02 22:47:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,7 +21,7 @@ class Triangle
     @sides.sort
     #raise the error if the lowest # is less than or equal to 0 or if the 2
     #lesser sides are less than or equal to the biggest side
-    raise TriangleError if sides[0] <= 0 or sides[0] + sides[1] <= sides[2]
+    raise TriangleError if @sides[0] <= 0 or @sides[0] + @sides[1] <= @sides[2]
   end
 end
 
